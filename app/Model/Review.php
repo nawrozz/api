60dba89f43b4b4ab0fb7d0393c9d6f524ce72f70
nawrozz <salehi.nawroz@gmail.com>
2020-07-14 20:30:08 +0430

    Relationship created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function Products(){
+        $this->belongsTo(Product::class);
+    }
 }
